6854e957754a08803ea6dc09f5ef9fbc121e343f
Ebeldev <ebelanger@hotmail.com>
2017-03-13 22:43:42 -0400

    first cap commit

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,18 +1,18 @@
 # Set the deployment directory on the target hosts.
-set :deploy_to, "/home/username/sites/#{application}-#{stage}" #FIXME
+set :deploy_to, "/home/etiennebel/public_html/sites/#{application}-#{stage}" #changed
 
 # Use the correct branch on github. Uncomment this if you have set up seperate branches for each staging area
 set :branch, "dev" #FIXME
 
 # The hostnames to deploy to.
-role :web, "yourdomain.com" #FIXME
+role :web, "etiennebelanger.com" #changed
 
 # Specify one of the web servers to use for database backups or updates.
 # This server should also be running Wordpress.
-role :db, "yourdomain.com", :primary => true #FIXME
+role :db, "etiennebelanger.com", :primary => true #changed
 
 # The path to wp-cli
 set :wp, "cd #{current_path}/#{app_root} ; /usr/bin/wp" #FIXME (Should be the full path to the wp command on your server)
 
 # The username on the target system, if different from your local username
-ssh_options[:user] = 'username' #FIXME
+ssh_options[:user] = 'etiennebel' #changed
